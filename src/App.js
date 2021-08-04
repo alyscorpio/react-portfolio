fff583dcdde16a2b8c2cf632c41590d876f44fd3
Alys Scorpio <alyssad1182@gmail.com>
2021-08-04 00:44:42 -0700

    creating pages and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { AuthProvider } from './utils/auth';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Work from './pages/Work/Work';
+import Contact from './pages/Contact/Contact';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/about" component={About} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/contact" component={Contact} />
+          {/* <ProtectedRoute exact path="/profile" onLoginFail="/login" component={Profile} /> */}
+          {/* <ProtectedRoute exact path="/results" onLoginFail="/login" component={Results} /> */}
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 }
 
