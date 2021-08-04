5d151bac4c352cc032282c01280119a03e2f8d17
Alys Scorpio <alyssad1182@gmail.com>
2021-08-04 01:14:22 -0700

    adding content to contact component

---
+++ src/components/contact/contact.jsx
@@ -0,0 +1,16 @@
+import "./about.scss";
+
+export default function About() {
+    return (
+        <div className="about" id="about">
+            <div className="label3">
+                <h2>Contact me</h2>
+            </div>
+            <div className="links">
+                <a class="links" href="mailto:alyssad1182@gmail.com">Email</a>
+                <a class="links" href="https://github.com/alyscorpio" target="_blank">GitHub</a>
+                <a class="links" href="www.linkedin.com/in/alysdickerson" target="_blank">LinkedIn</a>
+            </div>
+      </div>
+    );
+}
